026314658519aa3d363826f1af8ecab18a6b7cdb
vivek shelke <Vivekshelke12jul@gmail.com>
2026-04-01 12:40:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+
+const promiseFunction = (data) => {
+    console.log('Processing data...');
+    console.log(data);
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            if(data.age<18){
+                reject();
+            }else{
+                resolve();
+            }
+        }, 4000);
+    });
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = {
+        name: formData.get('name'),
+        age: Number(formData.get('age'))
+    }
+    promiseFunction(data)
+        .then(() => {
+            alert(`Welcome, ${data.name}. You can vote.`);
+        })
+        .catch(()=>{
+            alert(`Oh sorry ${data.name}. You aren't old enough.`);
+        });
+}
